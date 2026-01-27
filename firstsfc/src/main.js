1146628658c5fd4197c322342e052d1b800a0aee
BRI-09 <brenethananayo@gmail.com>
2026-01-27 15:31:10 +0800

    task 3

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,6 +6,8 @@ import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
 import FoodItem4 from './components/FoodItem4.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import Comment from './components/Comment.vue'
+import CommentForm from './components/CommentForm.vue'
 
 const app = createApp(App)
 
@@ -13,6 +15,9 @@ app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('food-item3', FoodItem3)
 app.component('food-item4', FoodItem4)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+
 // Fixed 'pp' to 'app' below
 app.component('personal-profile', PersonalProfile) 
 
